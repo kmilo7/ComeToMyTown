ec089ddd622c429bd3d7ac1f295d64f2bce6e972
Melissa <edisjara.2003@gmail.com>
2023-05-19 20:35:55 -0500

    Splash Created

--- app/src/main/java/com/example/cometomytown/MainActivity.java
+++ app/src/main/java/com/example/cometomytown/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.cometomytown;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+//Atributos = Variables
+    MediaPlayer song;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        song = MediaPlayer.create(this,R.raw.audiobg);
+        song.start();
+
+        //Timer Launch Song
+
+        TimerTask startApp = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Home.class);
+                startActivity(intent);
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(startApp,15000);
     }
 }
